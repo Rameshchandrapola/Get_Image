5725f1d8b13cebc9bc9983ef3804d9c6ee3e81dd
rameshchandra22 <pola.rameshchandra22@gmail.com>
2021-12-05 21:52:49 +0530

    add get_image

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Get_Image
-It's an Image generator from your search results using Unsplash API.
+##[Get Image]()
+It's an Image generator from your search results.
+###[Demo]()
+![](https://user-images.githubusercontent.com/76244600/144753825-56dd3529-5017-488f-b0f1-16866ca582ee.png)
+
+###Features:
+- A complete Working prototype.
+- Responsive.
+- Search bar.
+- Double Click to open image in new tab.
+- High quality images.
+- Load more images option.
+- Easy to use.
+- Social media support.
+
+### Built using
+- HTML
+- CSS
+- JavaScript
+- [Unsplash API](https://unsplash.com/developers) (The Unsplash API is a modern JSON API that surfaces all of the info you’ll need to build any experience for your users.)
+
+##Connect with me
+- [Twitter](https://twitter.com/POLARAMESHCHAN2)
+- [Instagram](https://www.instagram.com/polarameshchandra/)
+- [LinkedIn](https://www.linkedin.com/in/pola-ramesh-chandra-b78347202/)
---
+++ index.js
@@ -0,0 +1,79 @@
+const input = document.getElementById('search');
+const searchbtn = document.getElementById('searchbtn');
+const loadmore = document.querySelector('.load_more');
+
+input.addEventListener("keydown", event => {
+    if (event.key == "Enter") {
+        document.querySelector(".grid").textContent = "";
+        searchimage();
+    }
+})
+searchbtn.addEventListener("click", () => {
+    document.querySelector(".grid").textContent = "";
+    searchimage();
+})
+var page = 1;
+loadmore.addEventListener("click", () => {
+    page++;
+    if (input.value == "") {
+        defaultimages();
+    } else {
+        searchimage();
+    }
+});
+if (input.value == "") {
+    let url = "https://api.unsplash.com/search/photos/?query=ink&per_page=15&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
+    fetch(url)
+        .then(responce => {
+            if (responce.ok) {
+                return responce.json();
+            } else {
+                alert("Enter a valid search");
+            }
+        })
+
+        .then(data => {
+            for (let i = 0; i < data.results.length; i++) {
+                let image = document.createElement("div");
+                image.className = "img";
+                image.style.backgroundImage = "url(" + data.results[i].urls.raw + ")";
+                image.addEventListener("dblclick", function () {
+                    window.open(data.results[i].links.download, '_blank');
+                })
+                document.querySelector(".grid").appendChild(image);
+            }
+        })
+}
+
+searchimage = () => {
+    let url = "https://api.unsplash.com/search/photos/?query=" + input.value + "&per_page=15&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
+    fetch(url)
+        .then(responce => {
+            if (responce.ok) {
+                return responce.json();
+            } else {
+                alert("Enter a valid search");
+            }
+        })
+
+        .then(data => {
+            for (let i = 0; i < data.results.length; i++) {
+                let image = document.createElement("div");
+                image.className = "img";
+                image.style.backgroundImage = "url(" + data.results[i].urls.raw + ")";
+                image.addEventListener("dblclick", function () {
+                    window.open(data.results[i].links.download, '_blank');
+                })
+                document.querySelector(".grid").appendChild(image);
+            }
+        })
+
+        .catch(error => alert(error));
+}
+
+
+
+
+
+
+
