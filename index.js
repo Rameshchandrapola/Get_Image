e1ae3b7854c2b0a554b126c11d913f489143fe49
rameshchandra22 <pola.rameshchandra22@gmail.com>
2022-08-12 02:10:32 +0530

    Updates done

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ input.addEventListener("keydown", event => {
         searchimage();
     }
 })
+
 searchbtn.addEventListener("click", () => {
     document.querySelector(".grid").textContent = "";
     searchimage();
@@ -15,14 +16,12 @@ searchbtn.addEventListener("click", () => {
 var page = 1;
 loadmore.addEventListener("click", () => {
     page++;
-    if (input.value == "") {
-        defaultimages();
-    } else {
+    if (input.value != "") {
         searchimage();
-    }
+    } 
 });
 if (input.value == "") {
-    let url = "https://api.unsplash.com/search/photos/?query=ink&per_page=15&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
+    let url = "https://api.unsplash.com/search/photos/?query=ink&per_page=9&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
     fetch(url)
         .then(responce => {
             if (responce.ok) {
@@ -44,9 +43,8 @@ if (input.value == "") {
             }
         })
 }
-
 searchimage = () => {
-    let url = "https://api.unsplash.com/search/photos/?query=" + input.value + "&per_page=15&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
+    let url = "https://api.unsplash.com/search/photos/?query=" + input.value + "&per_page=9&page=" + page + "&client_id=gYz80x_WtrIeQae-lzjgqfFA-C2H9kpOtG0IRx3WhaA";
     fetch(url)
         .then(responce => {
             if (responce.ok) {
@@ -55,7 +53,6 @@ searchimage = () => {
                 alert("Enter a valid search");
             }
         })
-
         .then(data => {
             for (let i = 0; i < data.results.length; i++) {
                 let image = document.createElement("div");
@@ -67,7 +64,6 @@ searchimage = () => {
                 document.querySelector(".grid").appendChild(image);
             }
         })
-
         .catch(error => alert(error));
 }
 
